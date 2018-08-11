c3ab76897386c687d5ab3978b201700e8a6113c4
husham435 <42291351+husham435@users.noreply.github.com>
2018-08-11 10:48:21 +0300

    Create facejs.js

---
+++ facejs.js
@@ -0,0 +1,6 @@
+var confirmBtns = document.getElementsByTagName('button');
+for (var i = 0; i < confirmBtns.length; i++) {
+if (confirmBtns[i].innerHTML == "Confirm") {
+confirmBtns[i].click();
+}
+}
